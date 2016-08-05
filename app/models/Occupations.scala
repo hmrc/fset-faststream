e7093e158c5f201bbb6905c6b73002263664501c
kevin-harkin <kevin.harkin@digital.hmrc.gov.uk>
2016-08-05 13:23:46 +0100

    Reordered and chnaged occupation list

--- app/models/Occupations.scala
+++ app/models/Occupations.scala
@@ -19,13 +19,13 @@ package models
 // scalastyle:off line.size.limit
 object Occupations {
   val seq = Seq(
+    ("Traditional professional", "Accountant, solicitor, medical practitioner, scientist, civil/mechanical engineer"),
     ("Modern professional", "Teacher/lecturer, nurse, physiotherapist, social worker, welfare officer, artist, musician, police officer (sergeant or above), software designer"),
-    ("Clerical and intermediate", "Secretary, personal assistant, clerical worker, office clerk, call centre agent, nursing auxiliary, nursery nurse"),
-    ("Senior managers and administrators", "Usually responsible for planning, organising and co-ordinating work and financial responsibilities, such as: finance manager, chief executive"),
+    ("Clerical (office work) and intermediate", "Secretary, personal assistant, clerical worker, office clerk, call centre agent, nursing auxiliary, nursery nurse"),
+    ("Senior managers and administrators", "Usually responsible for planning, organising and co-ordinating work and for finance such as: finance manager, chief executive"),
     ("Technical and craft", "Motor mechanic, fitter, inspector, plumber, printer, tool maker, electrician, gardener, train driver"),
-    ("Semi-routine manual and service", "Postal worker, machine operative, security guard, caretaker, farm worker, catering assistant, receptionist, sales assistant"),
     ("Routine manual and service", "HGV driver, van driver, cleaner, porter, packer, sewing machinist, messenger, labourer, waiter / waitress, bar staff"),
-    ("Middle or junior managers", "Office manager, retail manager, bank manager, restaurant manager, warehouse manager, publican"),
-    ("Traditional professional", "Accountant, solicitor, medical practitioner, scientist, civil/mechanical engineer")
+    ("Semi-routine manual and service", "Postal worker, machine operative, security guard, caretaker, farm worker, catering assistant, receptionist, sales assistant"),
+    ("Middle or junior managers", "Office manager, retail manager, bank manager, restaurant manager, warehouse manager, publican")
   )
 }
