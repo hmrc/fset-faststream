6e70a810ffa5830ac5ad609a4acd7962f182661d
Oliver Wickham <mr.o.wickham@gmail.com>
2016-08-31 17:19:48 +0000

    Tweak the tests to test the integration with the global object Changed the tests to use the route helper, so we are not just retesting that the whitelistfilter library works in standalone mode, but that it works in our application

--- test/config/WhitelistFilterConfigSpec.scala
+++ test/config/WhitelistFilterConfigSpec.scala
@@ -22,64 +22,50 @@ import java.util.Base64
 import play.api.test._
 import org.scalatest.TestData
 import play.api.test.Helpers._
-import play.api.mvc.{ Action, Result }
-import play.api.mvc.Results._
-
-import play.api.http.Status._
-
-
 
 class WhitelistFilterConfigSpec extends PlaySpec with OneAppPerTest {
 
   implicit override def newAppForTest(td: TestData): FakeApplication =
     FakeApplication(additionalConfiguration = Map(
-        "whitelistExcludedCalls" -> Base64.getEncoder.encodeToString("/ping/pong,/healthcheck".getBytes),
+        "whitelistExcludedCalls" -> Base64.getEncoder.encodeToString("/ping/ping,/healthcheck".getBytes),
         "whitelist" -> Base64.getEncoder.encodeToString("11.22.33.44".getBytes)
-      )
-
-    )
+      ),
+     withGlobal = Some(ProductionFrontendGlobal))
 
   "FrontendAppConfig" must {
     "return a valid config item" when {
       "the whitelist exclusion paths are requested" in {
-
-          FrontendAppConfig.whitelistExcluded mustBe Seq("/ping/pong", "/healthcheck")
+        FrontendAppConfig.whitelistExcluded mustBe Seq("/ping/ping", "/healthcheck")
       }
       "the whitelist IPs are requested" in {
-          FrontendAppConfig.whitelist mustBe Seq("11.22.33.44")
+        FrontendAppConfig.whitelist mustBe Seq("11.22.33.44")
+      }
+    }
+  }
+
+  "ProductionFrontendGlobal" must {
+    "let requests passing" when {
+      "coming from an IP in the white list" in {
+        val request = FakeRequest(GET, "/fset-fast-stream/signup").withHeaders("True-Client-IP" -> "11.22.33.44")
+        val Some(result) = route(app, request)
+
+        status(result) mustBe (OK)
       }
 
-    }
+      "coming from an IP NOT in the white list, not in an excluded path" in {
+        val request = FakeRequest(GET, "/fset-fast-stream/signup").withHeaders("True-Client-IP" -> "93.00.33.33")
+        val Some(result) = route(app, request)
 
-    "WhitelistFilter" must {
-      "let requests passing" when {
-        "coming from an IP in the white list" in {
-          val underTest = WhitelistFilter
-          // Play.current is set at this point...
-          val rh = FakeRequest(GET, "/signup").withHeaders("True-Client-IP" -> "11.22.33.44")
-          val action = Action(Ok("success"))
-          val result = underTest(action)(rh).run
+        // Because of the redirect to the landing page
+        status(result) mustBe (SEE_OTHER)
+        redirectLocation(result) mustBe Some("https://www.tax.service.gov.uk/outage-fset-faststream/index.html")
+      }
 
-          status(result) mustBe (OK)
+      "coming from an IP NOT in the white list, but with white-listed path" in {
+        val request = FakeRequest(GET, "/ping/ping").withHeaders("True-Client-IP" -> "93.00.33.33")
+        val Some(result) = route(app, request)
 
-          // Caused by: java.lang.Exception: metrics plugin is not configured
-            /*running(TestServer(33337)) {
-
-              val response = await(WS.url("http://localhost:33337/signup").withHeaders("True-Client-IP" -> "11.22.33.44").get())
-              response.status mustBe(OK)
-            }*/
-        }
-
-        "coming from an IP NOT in the white list" in {
-          val underTest = WhitelistFilter
-          // Play.current is set at this point...
-          val rh = FakeRequest(GET, "/signup").withHeaders("True-Client-IP" -> "93.00.33.33")
-          val action = Action(Ok("success"))
-          val result = underTest(action)(rh).run
-
-          // Because of the redirect to the landing page
-          status(result) mustBe (SEE_OTHER)
-        }
+        status(result) mustBe (OK)
       }
     }
   }
