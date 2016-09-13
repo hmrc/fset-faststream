cc35a760dcdc867051997dacdb494ef51fc27bde
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2016-09-13 17:10:56 +0100

    Expose operations executed by Cubiks Callbacks

---
+++ app/controllers/OnlineExercisesController.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import model.exchange.OnlineTestReportReady
+import play.api.mvc.Action
+import uk.gov.hmrc.play.microservice.controller.BaseController
+import play.api.Logger
+import scala.concurrent.Future
+
+object OnlineExercisesController extends OnlineExercisesController {
+
+}
+
+trait OnlineExercisesController extends BaseController {
+
+  def startAssessment(assessmentId: String) = Action.async(parse.json) { implicit request =>
+    Logger.info(s"Assessment $assessmentId started")
+    Future.successful(Ok)
+  }
+
+  def completeAssessment(assessmentId: String) = Action.async(parse.json) { implicit request =>
+    Logger.info(s"Assessment $assessmentId completed")
+    Future.successful(Ok)
+  }
+
+  def markResultsReady(assessmentId: String) = Action.async(parse.json) { implicit request =>
+    withJsonBody[OnlineTestReportReady] { onlineTestReportReady =>
+      Logger.info(s"Assessment $assessmentId has report [$onlineTestReportReady] ready to download")
+      Future.successful(Ok)
+    }
+  }
+
+}
