dc076f3ca232d3fcce2f7d194f04796def8a9db2
satishkamatam <satish.kamatam@gmail.com>
2016-09-23 14:01:37 +0100

    Fix test generator to lookup progress status

--- app/controllers/testdata/TestDataGeneratorController.scala
+++ app/controllers/testdata/TestDataGeneratorController.scala
@@ -122,7 +122,7 @@ trait TestDataGeneratorController extends BaseController {
 
     TestDataGeneratorService.createCandidatesInSpecificStatus(
       numberToGenerate,
-      StatusGeneratorFactory.getGenerator(applicationStatus, progressStatus.map(ProgressStatuses.nameToProgressStatus)),
+      StatusGeneratorFactory.getGenerator(applicationStatus, progressStatus.flatMap(ps => ProgressStatuses.nameToProgressStatus.get(ps))),
       initialConfig
     ).map { candidates =>
       Ok(Json.toJson(candidates))
