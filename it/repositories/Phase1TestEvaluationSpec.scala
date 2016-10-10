3d34e2cefd2eb693ca29fc77bc9b1642c36f3196
satishkamatam <satish.kamatam@gmail.com>
2016-10-10 12:55:33 +0100

    phase1 pass mark evaluation tests

---
+++ it/repositories/Phase1TestEvaluationSpec.scala
@@ -0,0 +1,155 @@
+package repositories
+
+import config.{ CubiksGatewayConfig, Phase1TestsConfig }
+import factories.DateTimeFactory
+import model.ApplicationStatus
+import model.ApplicationStatus.{ apply => _, _ }
+import model.EvaluationResults._
+import model.SchemeType._
+import model.exchange.passmarksettings.{ PassMarkThreshold, Phase1PassMark, Phase1PassMarkSettings, Phase1PassMarkThresholds }
+import model.persisted.SchemeEvaluationResult
+import org.joda.time.DateTime
+import org.mockito.Mockito._
+import org.scalatest.mock.MockitoSugar
+import org.scalatest.prop._
+import repositories.application.GeneralApplicationMongoRepository
+import repositories.assistancedetails.AssistanceDetailsMongoRepository
+import repositories.onlinetesting.{ Phase1EvaluationMongoRepository, Phase1TestMongoRepository }
+import services.GBTimeZoneService
+import services.onlinetesting.EvaluatePhase1ResultService
+import testkit.MongoRepositorySpec
+
+import scala.Double.NaN
+
+
+class Phase1TestEvaluationSpec extends MongoRepositorySpec with CommonRepository with MockitoSugar
+  with TableDrivenPropertyChecks {
+  val collectionName = "application"
+
+  val mockGatewayConfig = mock[CubiksGatewayConfig]
+
+  def applicationRepository = new GeneralApplicationMongoRepository(GBTimeZoneService, mockGatewayConfig)
+
+  def schemePreferencesRepository = new schemepreferences.SchemePreferencesMongoRepository
+
+  def assistanceDetailsRepository = new AssistanceDetailsMongoRepository
+
+  def phase1TestRepository = new Phase1TestMongoRepository(DateTimeFactory)
+
+  def phase1PassMarkSettingRepo = new Phase1PassMarkSettingsMongoRepository()
+
+  def phase1EvaluationRepo = new Phase1EvaluationMongoRepository()
+
+  def phase1TestEvaluationService = new EvaluatePhase1ResultService {
+    val phase1EvaluationRepository = phase1EvaluationRepo
+    val gatewayConfig = mockGatewayConfig
+    val phase1PMSRepository = phase1PassMarkSettingRepo
+    val phase1TestsConfigMock = mock[Phase1TestsConfig]
+
+    when(gatewayConfig.phase1Tests).thenReturn(phase1TestsConfigMock)
+    when(phase1TestsConfigMock.scheduleIds).thenReturn(Map("sjq" -> 1, "bq" -> 2))
+  }
+
+  val phase1PassMarkSettingsTable = Table[SchemeType, Double, Double, Double, Double](
+    ("Scheme Name", "SJQ Fail Threshold", "SJQ Pass threshold", "BQ Fail Threshold", "BQ Pass Threshold"),
+    (Commercial, 20.0, 80.0, 30.0, 70.0),
+    (DigitalAndTechnology, 20.001, 20.001, 20.01, 20.05),
+    (DiplomaticService, 20.01, 20.02, 20.01, 20.02),
+    (DiplomaticServiceEconomics, 30.0, 70.0, 30.0, 70.0),
+    (DiplomaticServiceEuropean, 30.0, 70.0, 30.0, 70.0),
+    (European, 30.0, 70.0, 30.0, 70.0),
+    (Finance, 30.0, 70.0, 30.0, 70.0),
+    (Generalist, 30.0, 70.0, 30.0, 70.0),
+    (GovernmentCommunicationService, 30.0, 70.0, 30.0, 70.0),
+    (GovernmentEconomicsService, 30.0, 70.0, 30.0, 70.0),
+    (GovernmentOperationalResearchService, 30.0, 70.0, 30.0, 70.0),
+    (GovernmentSocialResearchService, 30.0, 70.0, 30.0, 70.0),
+    (GovernmentStatisticalService, 30.0, 70.0, 30.0, 70.0),
+    (HousesOfParliament, 30.0, 70.0, 30.0, 70.0),
+    (HumanResources, 30.0, 70.0, 30.0, 70.0),
+    (ProjectDelivery, 30.0, 70.0, 30.0, 70.0),
+    (ScienceAndEngineering, 30.0, 70.0, 30.0, 70.0)
+  )
+
+  def applicationTable(application: (String, Double, Double, Boolean, List[SchemeType])*) = Table(
+    ("ApplicationId", "SJQ Score", "BQ Score", "isGis", "Selected Schemes"),
+    application: _*)
+
+  def expectedResultTable(expectedResult: (String, ApplicationStatus, List[(SchemeType, Result)])*) = Table(
+    ("Application Id", "Application Status", "Expected schemes And Results"),
+    expectedResult: _*)
+
+  "phase1 evaluation process" should {
+    "process phase1 test results for candidates that has completed phase1 tests" in {
+      val applications = applicationTable(
+        ("application-1", 25, 80, false, List(Commercial, DigitalAndTechnology)),
+        ("application-2", 20.001, 25, false, List(Commercial, DigitalAndTechnology)),
+        ("application-3", 20.015, 20.015, false, List(Commercial, DigitalAndTechnology))
+      )
+      val expectedResults = expectedResultTable(
+        ("application-1", PHASE1_TESTS_PASSED, List(Commercial -> Amber, DigitalAndTechnology -> Green)),
+        ("application-2", PHASE1_TESTS_PASSED, List(Commercial -> Red, DigitalAndTechnology -> Green)),
+        ("application-3", PHASE1_TESTS, List(Commercial -> Red, DigitalAndTechnology -> Amber))
+      )
+      evaluatePhase1Tests(applications)(expectedResults)
+    }
+
+    "process phase1 test results for gis candidates" in {
+      val applications = applicationTable(
+        ("application-1", 25, NaN, true, List(Commercial, DigitalAndTechnology))
+      )
+      val expectedResults = expectedResultTable(
+        ("application-1", PHASE1_TESTS_PASSED, List(Commercial -> Amber, DigitalAndTechnology -> Green))
+      )
+      evaluatePhase1Tests(applications)(expectedResults)
+    }
+  }
+
+  private def createPhase1PassMarkSettings(): Phase1PassMarkSettings = {
+    val schemeThresholds = phase1PassMarkSettingsTable.map {
+      fields => Phase1PassMark(fields._1,
+        Phase1PassMarkThresholds(PassMarkThreshold(fields._2, fields._3), PassMarkThreshold(fields._4, fields._5)))
+    }.toList
+
+    val phase1PassMarkSettings = Phase1PassMarkSettings(
+      schemeThresholds,
+      "version-1",
+      DateTime.now,
+      "user-1"
+    )
+    phase1PassMarkSettingRepo.create(phase1PassMarkSettings).futureValue
+    phase1PassMarkSettingRepo.getLatestVersion.futureValue.get
+  }
+
+  private def evaluatePhase1Tests(applications: TableFor5[String, Double, Double, Boolean, List[SchemeType]])
+                                 (expectedPhase1Results: TableFor3[String, ApplicationStatus, List[(SchemeType, Result)]]) = {
+
+    val phase1PassMarkSettings = createPhase1PassMarkSettings()
+
+    val actualPhase1Results = applications.map {
+      case (applicationId, sjqScore, bqScore, isGis, selectedSchemes) =>
+        val optBqScore = if (bqScore.isNaN) None else Some(bqScore)
+
+        val applicationReadyForEvaluation = insertApplicationWithPhase1TestResults(applicationId, sjqScore, optBqScore,
+          isGis)(selectedSchemes: _*)
+
+        phase1TestEvaluationService.evaluate(applicationReadyForEvaluation, phase1PassMarkSettings).futureValue
+
+        val passMarkEvaluation = phase1EvaluationRepo.getPassMarkEvaluation(applicationId).futureValue
+
+        val applicationStatus = ApplicationStatus.withName(applicationRepository.findStatus(applicationId).futureValue.status)
+
+        val schemeResults = passMarkEvaluation.result.map {
+          SchemeEvaluationResult.unapply(_).map {
+            case (schemeType, resultStr) => schemeType -> Result(resultStr)
+          }.get
+        }
+
+        (applicationId, applicationStatus, schemeResults)
+    }
+
+    actualPhase1Results must contain theSameElementsAs expectedPhase1Results
+
+  }
+
+}
