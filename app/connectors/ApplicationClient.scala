ea0ea5e560ab63ce40ff610414776ae4b010a186
Ben Stephenson <bjames.stephenson@gmail.com>
2016-10-12 17:00:38 +0100

    Remove unused import statement

--- app/connectors/ApplicationClient.scala
+++ app/connectors/ApplicationClient.scala
@@ -21,7 +21,6 @@ import connectors.exchange.PartnerGraduateProgrammes._
 import connectors.exchange.Questionnaire._
 import connectors.exchange._
 import models.UniqueIdentifier
-import play.api.Logger
 import play.api.http.Status._
 import play.api.libs.json.Json
 import uk.gov.hmrc.play.http._
