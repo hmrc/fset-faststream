315c7af2123aaf87eb6fd6e8e13f1d6bafd9b98e
satishkamatam <satish.kamatam@gmail.com>
2016-10-13 16:35:31 +0100

    missing routes added

--- app/repositories/onlinetesting/OnlineTestRepository.scala
+++ app/repositories/onlinetesting/OnlineTestRepository.scala
@@ -52,7 +52,7 @@ trait OnlineTestRepository[U <: Test, T <: TestProfile[U]] extends RandomSelecti
       "$elemMatch" -> BSONDocument("token" -> token)
     ))
 
-    phaseTestProfileByQuery(query).map { x =>
+    phaseTestProfileByQuery(query, phase).map { x =>
       x.getOrElse(cannotFindTestByToken(token))
     }
   }
