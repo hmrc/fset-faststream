e805826fa9b4481535db85202746f5e1e10a14f2
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2016-10-24 18:15:28 +0100

    style

--- test/model/ProgressStatusesSpec.scala
+++ test/model/ProgressStatusesSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package model
 
 import org.scalatestplus.play.PlaySpec
@@ -5,7 +21,6 @@ import services.testdata.ApplicationStatusOnlyForTest
 
 class ProgressStatusesSpec extends PlaySpec {
 
-  // scalastyle:off
   "Progress statuses" should {
     "be assigned to all application statuses" in {
       object JustForTest extends Enumeration with ApplicationStatusOnlyForTest {
