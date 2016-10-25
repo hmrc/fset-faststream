7c70d882eb7faa3dc4d4eb65e9e02d3d39975a5b
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2016-10-25 11:30:56 +0100

    Correct inconsistencies

--- app/controllers/CubiksTestController.scala
+++ app/controllers/CubiksTestController.scala
@@ -46,19 +46,6 @@ abstract class CubiksTestController(applicationClient: ApplicationClient, cacheC
       }
   }
 
-  // TODO: permission this endpoint
-  def startPhase3Tests = CSRSecureAppAction(Phase3TestInvitedRole) { implicit request =>
-    implicit cachedUserData =>
-      applicationClient.getPhase3TestGroup(cachedUserData.application.applicationId).flatMap { testProfile =>
-        // If we've started but not completed a test we still want to send them to that
-        // test link to continue with it
-        testProfile.tests.find(!_.completed).map { testToStart =>
-          // applicationClient.startTest(testToStart.cubiksUserId)
-          Future.successful(Redirect(testToStart.testUrl))
-        }.getOrElse(Future.successful(NotFound))
-      }
-  }
-
   def completeSjqByTokenAndContinuePhase1Tests(token: UniqueIdentifier) = CSRUserAwareAction { implicit request =>
     implicit user =>
       applicationClient.completeTestByToken(token).map { _ =>
