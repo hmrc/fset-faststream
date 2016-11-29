5952ed59b3b1a28ed5a08e8952f7ec95653d6c67
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2016-11-29 15:21:50 +0000

    Add contact details

--- app/services/parity/ParityExportService.scala
+++ app/services/parity/ParityExportService.scala
@@ -18,7 +18,7 @@ package services.parity
 
 import config.{ MicroserviceAppConfig, ParityGatewayConfig }
 import play.api.Logger
-import play.api.libs.json.JsObject
+import play.api.libs.json.{ JsObject, JsValue }
 import services.events.{ EventService, EventSink }
 import repositories._
 import repositories.parity.ParityExportRepository
@@ -30,6 +30,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object ParityExportService extends ParityExportService {
   val eventService = EventService
   val parityExRepository = parityExportRepository
+  val cdRepository = faststreamContactDetailsRepository
   val parityGatewayConfig = MicroserviceAppConfig.parityGatewayConfig
 }
 
@@ -37,13 +38,22 @@ trait ParityExportService extends EventSink {
 
   val parityExRepository: ParityExportRepository
   val parityGatewayConfig: ParityGatewayConfig
+  val cdRepository: contactdetails.ContactDetailsRepository
 
   // Random apps in PHASE3_TESTS_PASSED_NOTIFIED
   def nextApplicationsForExport(batchSize: Int): Future[List[String]] = parityExRepository.nextApplicationsForExport(batchSize)
 
+  // scalastyle:off method.length
   def exportApplication(applicationId: String): Future[Unit] = {
-    parityExRepository.getApplicationForExport(applicationId).map { applicationDoc =>
 
+    try {
+    (for {
+      applicationDoc <- parityExRepository.getApplicationForExport(applicationId)
+      userId = (applicationDoc \ "userId").as[String]
+      _ = print("User ID = " + userId + "\n")
+      contactDetailsDoc <- cdRepository.find(userId)
+      // _ = print("Contact Details = " + contactDetailsDoc + "\n")
+    } yield {
       Logger.debug("============ App = " + applicationDoc)
 
       /*
@@ -54,14 +64,58 @@ trait ParityExportService extends EventSink {
         )
       )*/
 
+      def optionalItem(key: String, valueOpt: Option[JsValue]): String = valueOpt.map { value =>
+        s""""$key": $value,"""
+      }.getOrElse("")
+
+      val personalDetails = applicationDoc \ "personal-details"
+      val civilServiceExperienceDetails = applicationDoc \ "civil-service-experience-details"
+
       val export2 =
         s"""
+          | "token": ${parityGatewayConfig.upstreamAuthToken},
           | "application": {
-          |   "token": ${parityGatewayConfig.upstreamAuthToken},
-          |   "userId": ${applicationDoc \ "userId"}
+          |
+          |   "userId": "$userId",
+          |   "applicationId": ${applicationDoc \ "applicationId"},
+          |   "contact-details": {
+          |     "outsideUk": ${contactDetailsDoc.outsideUk},
+          |     "country": "${contactDetailsDoc.country}",
+          |     "address": {
+          |       "line1": "${contactDetailsDoc.address.line1}",
+          |       "line2": "${contactDetailsDoc.address.line2}",
+          |       "line3": "${contactDetailsDoc.address.line3}",
+          |       "line4": "${contactDetailsDoc.address.line4}"
+          |     },
+          |     "email": "${contactDetailsDoc.email}",
+          |     "phone": "${contactDetailsDoc.phone}",
+          |     "postCode": "${contactDetailsDoc.postCode}",
+          |     "frameworkId": ${applicationDoc \ "frameworkId"},
+          |     "personal-details": {
+          |       "firstName": ${personalDetails \ "firstName"},
+          |       "lastName": ${personalDetails \ "lastName"},
+          |       "preferredName": ${personalDetails \ "preferredName"},
+          |       "dateOfBirth": ${personalDetails \ "dateOfBirth"}
+          |     },
+          |     "civil-service-experience-details": {
+          |       "applicable": ${civilServiceExperienceDetails \ "applicable"},
+          |       ${optionalItem("civilServiceExperienceType", (civilServiceExperienceDetails \ "civilServiceExperienceType").asOpt)}
+          |       "internshipTypes": ${civilServiceExperienceDetails \ "internshipTypes"},
+          |       "fastPassReceived": ${civilServiceExperienceDetails \ "fastPassReceived"}
+          |     }
+          |
+          |   }
           | }
         """.stripMargin
 
+      Logger.debug("=========== Exp = " + export2)
+    }).recover {
+      case _ => print("Error!!!!\n")
     }
+    } catch {
+      case _: Throwable => print("Exception!!!!\n"); Future.successful(())
+    }
+
+    // scalastyle:on method.length
   }
 }
--- test/services/parity/ParityExportServiceSpec.scala
+++ test/services/parity/ParityExportServiceSpec.scala
@@ -18,14 +18,20 @@ package services.parity
 
 import org.scalatestplus.play.OneAppPerSuite
 import testkit.UnitSpec
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class ParityExportServiceSpec extends UnitSpec with OneAppPerSuite {
 
   "Export Application" should {
     "Display some things" in new TestFixture {
-      sut.exportApplication("e4ed2087-20ad-4e4c-890b-7267196a62aa")
+      sut.exportApplication("e4ed2087-20ad-4e4c-890b-7267196a62aa").recover {
+        case _ => print("There was an error\n")
+      }
     }
   }
+
+  // e4ed2087-20ad-4e4c-890b-7267196a62aa
+  // 5ba7d069-7491-4bdf-ba4d-584eb7ef46be
 
   trait TestFixture {
     val sut = ParityExportService
