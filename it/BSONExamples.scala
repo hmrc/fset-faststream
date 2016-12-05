f8ae07399186d7ecc7453e1a84d7547d96cb5106
Miguel <mgilgar@gmail.com>
2016-12-05 13:27:12 +0000

    Added some unit testing for Candidate Progress Report for SDIP

---
+++ it/BSONExamples.scala
@@ -0,0 +1,61 @@
+package model
+
+import reactivemongo.bson.{ BSONArray, BSONDocument }
+
+object BSONExamples {
+  val SubmittedSdipCandidateWithEdipCompleted = {
+    BSONDocument(
+      "_id" -> "58454b6261ae24a4609f0e4f",
+      "applicationId" -> "a665043b-8317-4d28-bdf6-086859ac17ff",
+      "userId" -> "459b5e72-e004-48ff-9f00-adbddf59d9c4",
+      "frameworkId" -> "FastStream-2016",
+      "applicationStatus" -> "SUBMITTED",
+      "applicationRoute" -> "Sdip",
+      "progress-status" -> BSONDocument(
+        "personal-details" -> true,
+        "IN_PROGRESS" -> true,
+        "scheme-preferences" -> true,
+        "assistance-details" -> true,
+        "questionnaire" -> BSONDocument(
+          "start_questionnaire" -> true,
+          "education_questionnaire" -> true,
+          "diversity_questionnaire" -> true,
+          "occupation_questionnaire" -> true
+        ),
+        "preview" -> true,
+        "SUBMITTED" -> true
+      ),
+      "personal-details" -> BSONDocument(
+        "firstName" -> "Breana1",
+        "lastName" -> "Bailey1",
+        "preferredName" -> "PrefBreana1",
+        "dateOfBirth" -> "1981-05-21",
+        "edipCompleted" -> true
+      ),
+      "progress-status-timestamp" -> BSONDocument(
+        "IN_PROGRESS" -> "2016-12-05T11:11:31.787Z",
+        "SUBMITTED" -> "2016-12-05T11:11:31.869Z"
+      ),
+      "civil-service-experience-details" -> BSONDocument(
+        "applicable" -> true,
+        "civilServiceExperienceType" -> "CivilServant"
+      ),
+      "scheme-preferences" -> BSONDocument(
+        "schemes" -> BSONArray(
+          "Sdip"
+        ),
+        "orderAgreed" -> true,
+        "eligible" -> true
+      ),
+      "assistance-details" -> BSONDocument(
+        "hasDisability" -> "No",
+        "needsSupportForPhoneInterviewDescription" -> "I need good headphones",
+        "needsSupportForPhoneInterview" -> true,
+        "needsSupportAtVenueDescription" -> "I need a comfortable chair because of my back problem",
+        "needsSupportAtVenue" -> true,
+        "needsSupportForOnlineAssessment" -> false,
+        "guaranteedInterview" -> false
+      )
+    )
+  }
+}
