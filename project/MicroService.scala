4fa37b8df3bb02e5861d0ae0491b840f1668e1ef
Oliver Wickham <mr.o.wickham@gmail.com>
2016-12-05 10:58:06 +0000

    Add -Ywarn-value-discard scalac option for main compile

--- project/MicroService.scala
+++ project/MicroService.scala
@@ -58,7 +58,9 @@ trait MicroService {
       fork in Test := true,
       javaOptions in Test += "-Dmicroservice.services.user-management.url.host=http://localhost:11111",
       retrieveManaged := true,
-      scalacOptions += "-feature"
+      scalacOptions += "-feature",
+      // Currently don't enable warning in value discard in tests until ScalaTest 3
+      scalacOptions in (Compile, compile) += "-Ywarn-value-discard"
     )
     .settings(sources in (Compile, doc) := Seq.empty)
     .configs(IntegrationTest)
