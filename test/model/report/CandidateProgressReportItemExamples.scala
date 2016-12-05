5cdebe652b48e52ad50caca38192284cf48e3114
Miguel <mgilgar@gmail.com>
2016-12-05 14:26:10 +0000

    imports optimised

--- test/model/report/CandidateProgressReportItemExamples.scala
+++ test/model/report/CandidateProgressReportItemExamples.scala
@@ -17,9 +17,6 @@
 package model.report
 
 import model.SchemeType
-import model.report.{ ApplicationForOnlineTestPassMarkReportItem, TestResultsForOnlineTestPassMarkReportItem }
-
-import scala.util.Random
 
 object CandidateProgressReportItemExamples {
   lazy val SdipCandidate = CandidateProgressReportItem("459b5e72-e004-48ff-9f00-adbddf59d9c4", "a665043b-8317-4d28-bdf6-086859ac17ff",
