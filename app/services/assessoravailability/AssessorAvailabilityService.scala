9d2d8f05f158f437ae2e7ba591775701a58de5f6
Ian Stainer <ian.stainer@valtech.co.uk>
2017-06-02 11:03:09 +0100

    Tidy up

--- app/services/assessoravailability/AssessorAvailabilityService.scala
+++ app/services/assessoravailability/AssessorAvailabilityService.scala
@@ -30,14 +30,14 @@ trait AssessorAvailabilityService {
   val aaRepository: AssessorAvailabilityRepository
 
   def save(userId: String, assessorAvailability: model.exchange.AssessorAvailability): Future[Unit] = {
-    aaRepository.find(userId).flatMap{
-        case Some(existing) =>
-          val mergedAvailability = existing.availability ++ assessorAvailability.availability
-          val assessorAvailabilityToPersist = model.persisted.AssessorAvailability(userId, mergedAvailability)
-          aaRepository.save(assessorAvailabilityToPersist).map( _ => () )
-        case _ =>
-          val assessorAvailabilityToPersist = model.persisted.AssessorAvailability(userId, assessorAvailability.availability)
-          aaRepository.save(assessorAvailabilityToPersist).map( _ => () )
+    aaRepository.find(userId).flatMap {
+      case Some(existing) =>
+        val mergedAvailability = existing.availability ++ assessorAvailability.availability
+        val assessorAvailabilityToPersist = model.persisted.AssessorAvailability(userId, mergedAvailability)
+        aaRepository.save(assessorAvailabilityToPersist).map( _ => () )
+      case _ =>
+        val assessorAvailabilityToPersist = model.persisted.AssessorAvailability(userId, assessorAvailability.availability)
+        aaRepository.save(assessorAvailabilityToPersist).map( _ => () )
     }
   }
 
