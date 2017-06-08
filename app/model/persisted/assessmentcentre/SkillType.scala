256aff63a5dc0ae280f9d796912e5b70cd8d8946
fayimora <fayi@fayimora.com>
2017-06-08 11:55:05 +0100

    Setup domain models

---
+++ app/model/persisted/assessmentcentre/SkillType.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package model.persisted.assessmentcentre
+
+import play.api.libs.json.{ Format, JsString, JsSuccess, JsValue }
+import reactivemongo.bson.{ BSON, BSONHandler, BSONString }
+
+import scala.language.implicitConversions
+
+/**
+  * Created by fayimora on 08/06/2017.
+  */
+object SkillType extends Enumeration {
+  type SkillType = Value
+
+  val ASSESSOR, EXERCISE_MARKER, QUALITY_ASSURANCE_COORDINATOR, SIFTER = Value
+
+  implicit def toString(SkillType: SkillType): String = SkillType.toString
+
+  implicit val SkillTypeFormat = new Format[SkillType] {
+    def reads(json: JsValue) = JsSuccess(SkillType.withName(json.as[String].toUpperCase()))
+    def writes(myEnum: SkillType) = JsString(myEnum.toString)
+  }
+
+  implicit object BSONEnumHandler extends BSONHandler[BSONString, SkillType] {
+    def read(doc: BSONString) = SkillType.withName(doc.value.toUpperCase())
+    def write(stats: SkillType) = BSON.write(stats.toString)
+  }
+}
