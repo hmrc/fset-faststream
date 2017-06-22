5f7a52835441adbb7c58308d5507c324e4f8503c
Benji Stephenson <bjames.stephenson@gmail.com>
2017-06-22 13:52:42 +0100

    Add controller test for getting event

--- test/controllers/EventsControllerSpec.scala
+++ test/controllers/EventsControllerSpec.scala
@@ -17,6 +17,7 @@
 package controllers
 
 import config.TestFixtureBase
+import model.Exceptions.EventNotFoundException
 import model.persisted.eventschedules.Event
 import model.persisted.eventschedules.EventType
 import model.persisted.eventschedules.VenueType
@@ -25,6 +26,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import org.mockito.ArgumentMatchers.{ eq => eqTo, _ }
 import org.mockito.Mockito._
+import play.api.libs.json.Json
 import services.events.EventsService
 import testkit.UnitWithAppSpec
 
@@ -59,6 +61,22 @@ class EventsControllerSpec extends UnitWithAppSpec {
      "return 400 for invalid event or venue types" in new TestFixture {
        status(controller.fetchEvents("blah","LONDON_FSAC")(FakeRequest())) mustBe BAD_REQUEST
        status(controller.fetchEvents("FSAC","blah")(FakeRequest())) mustBe BAD_REQUEST
+    }
+
+    "return 200 for an event for an id" in new TestFixture {
+      val event = Event("id", EventType.FSAC, "London", VenueType.LONDON_FSAC, LocalDate.now, 30, 20, 5, LocalTime.now, LocalTime.now, Map.empty)
+      when(mockEventsService.getEvent(any[String])).thenReturn(Future.successful(event))
+
+      val result = controller.getEvent("id")(FakeRequest())
+      status(result) mustBe OK
+      contentAsJson(result) mustBe Json.toJson(event)
+    }
+
+    "return a 404 if no event is found" in new TestFixture {
+      when(mockEventsService.getEvent(any[String])).thenReturn(Future.failed(EventNotFoundException("")))
+      val result = controller.getEvent("id")(FakeRequest())
+
+      status(result) mustBe NOT_FOUND
     }
   }
 
