bf74f5b4fbbc02ba0a1da69c1e5aa0c82b0be1d9
Andrew Rembrandt <andrew@rembrandt.me.uk>
2017-07-18 11:05:01 +0100

    Interim changes

--- it/repositories/sift/ApplicationSiftRepositorySpec.scala
+++ it/repositories/sift/ApplicationSiftRepositorySpec.scala
@@ -1,8 +1,10 @@
 package repositories.sift
 
-import model.{ApplicationStatus, SchemeId}
+import model.{ApplicationStatus, ProgressStatuses, SchemeId}
 import model.command.ApplicationForSift
+import model.persisted.phase3tests.{LaunchpadTest, Phase3TestGroup}
 import model.persisted.{PassmarkEvaluation, SchemeEvaluationResult}
+import org.joda.time.DateTime
 import org.scalatest.concurrent.ScalaFutures
 import repositories.{CollectionNames, CommonRepository}
 import testkit.{MockitoSugar, MongoRepositorySpec}
@@ -17,11 +19,14 @@ class ApplicationSiftRepositorySpec extends MongoRepositorySpec with ScalaFuture
   "next Application for sift" should {
     "return a single matching application ready for progress to sift" in {
       insertApplicationWithPhase3TestNotifiedResults("appId1", Some(100), PassmarkEvaluation("", Some(""),
-        List(SchemeEvaluationResult(SchemeId("Commercial"), "passed")), "", Some("")))(SchemeId("Commercial"))
+        List(SchemeEvaluationResult(SchemeId("Commercial"), "Green")), "", Some("")))(SchemeId("Commercial"))
 
-      whenReady(applicationSiftRepository.nextApplicationsForSift(10)) { applications =>
-        applications mustBe List(ApplicationForSift("", PassmarkEvaluation("", None, Nil, "", None)))
-      }
+      phase3TestRepository.insertOrUpdateTestGroup("appId1", Phase3TestGroup(DateTime.now(), List(), Some(PassmarkEvaluation("", Some(""),
+        List(SchemeEvaluationResult(SchemeId("Commercial"), "Green")), "", Some("")))))
+
+      val f = applicationSiftRepository.nextApplicationsForSift(10)
+      f.futureValue mustBe
+        List(ApplicationForSift("", PassmarkEvaluation("", None, Nil, "", None)))
     }
   }
 }
