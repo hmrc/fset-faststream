0c132cd17b6416e8a6d1358ac80f0cb3ccf224c0
fayimora <fayi@fayimora.com>
2017-07-21 13:50:55 +0100

    Add endpoint for creating an event

--- app/controllers/EventsController.scala
+++ app/controllers/EventsController.scala
@@ -18,12 +18,12 @@ package controllers
 
 import model.Exceptions.{ EventNotFoundException, OptimisticLockException }
 import model.{ command, exchange }
-import model.exchange.AssessorAllocations
-import model.persisted.eventschedules.EventType
+import model.exchange.{ AssessorAllocations, Event => ExchangeEvent }
+import model.persisted.eventschedules.{ Event, EventType }
 import model.persisted.eventschedules.EventType.EventType
 import play.api.libs.json.{ JsValue, Json }
 import play.api.mvc.{ Action, AnyContent }
-import repositories.events.{ LocationsWithVenuesInMemoryRepository, LocationsWithVenuesRepository, UnknownVenueException }
+import repositories.events.{ EventsMongoRepository, LocationsWithVenuesInMemoryRepository, LocationsWithVenuesRepository, UnknownVenueException }
 import services.allocation.AssessorAllocationService
 import services.events.EventsService
 import uk.gov.hmrc.play.microservice.controller.BaseController
@@ -34,12 +34,15 @@ import scala.util.Try
 
 object EventsController extends EventsController {
   val eventsService: EventsService = EventsService
+  val eventsRepo: EventsMongoRepository = repositories.eventsRepository
   val locationsAndVenuesRepository: LocationsWithVenuesRepository = LocationsWithVenuesInMemoryRepository
   val assessorAllocationService: AssessorAllocationService = AssessorAllocationService
 }
 
 trait EventsController extends BaseController {
   def eventsService: EventsService
+
+  def eventsRepo: EventsMongoRepository
 
   def locationsAndVenuesRepository: LocationsWithVenuesRepository
 
@@ -48,6 +51,15 @@ trait EventsController extends BaseController {
   def saveAssessmentEvents(): Action[AnyContent] = Action.async { implicit request =>
     eventsService.saveAssessmentEvents().map(_ => Created("Events saved"))
       .recover { case e: Exception => UnprocessableEntity(e.getMessage) }
+  }
+
+  def createEvent() = Action.async(parse.json) { implicit request =>
+    withJsonBody[ExchangeEvent] { event =>
+      val persistedEvent = Event(event)
+      eventsRepo.save(persistedEvent :: Nil).map { _ =>
+        Created
+      }.recover { case e: Exception => UnprocessableEntity(e.getMessage) }
+    }
   }
 
   def getEvent(eventId: String): Action[AnyContent] = Action.async { implicit request =>
