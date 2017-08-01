eac50c7f20c837904a257885ab6142dfdd86830c
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2017-08-01 14:04:39 +0100

    Remove DRY in fileupload tests

--- it/repositories/fileupload/FileUploadRepositorySpec.scala
+++ it/repositories/fileupload/FileUploadRepositorySpec.scala
@@ -12,24 +12,25 @@ class FileUploadRepositorySpec extends MongoRepositorySpec {
   override val collectionName: String = CollectionNames.FILE_UPLOAD
   lazy val repository = repositories.fileUploadRepository
 
-  private def storeAndRetrieveFile = {
-    val testContent = "Test contents".toCharArray.map(_.toByte)
-    val testContentType = "application/pdf"
-    val id = repository.add(testContentType, testContent).futureValue
-    val retrievedFile = repository.retrieve(id).futureValue
-    retrievedFile.contentType mustBe testContentType
-    retrievedFile.fileContents.run(Iteratee.consume[Array[Byte]]()).futureValue mustBe testContent
-  }
-
   "add" must {
     "store a file with contentType" in {
-      storeAndRetrieveFile
+      val testContent = "Test contents".toCharArray.map(_.toByte)
+      val testContentType = "application/pdf"
+      val id = repository.add(testContentType, testContent).futureValue
+      val retrievedFile = repository.retrieve(id).futureValue
+      retrievedFile.contentType mustBe testContentType
+      retrievedFile.fileContents.run(Iteratee.consume[Array[Byte]]()).futureValue mustBe testContent
     }
   }
 
   "retrieve" must {
     "retrieve a file by id" in {
-      storeAndRetrieveFile
+      val testContent = "Test contents".toCharArray.map(_.toByte)
+      val testContentType = "application/pdf"
+      val id = repository.add(testContentType, testContent).futureValue
+      val retrievedFile = repository.retrieve(id).futureValue
+      retrievedFile.contentType mustBe testContentType
+      retrievedFile.fileContents.run(Iteratee.consume[Array[Byte]]()).futureValue mustBe testContent
     }
   }
 }
