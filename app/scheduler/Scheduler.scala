1c596a9024da33b5bed84e02c187920579e5dc7c
Henri Cook <henri.cook@digital.hmrc.gov.uk>
2017-08-04 13:30:14 +0100

    Enable evaluate assessment score job scheduler

--- app/scheduler/Scheduler.scala
+++ app/scheduler/Scheduler.scala
@@ -17,6 +17,7 @@
 package scheduler
 
 import play.api.Logger
+import scheduler.assessment.{ EvaluateAssessmentScoreJob, EvaluateAssessmentScoreJobConfig }
 import scheduler.fixer.{ FixerJob, FixerJobConfig }
 import scheduler.onlinetesting._
 import uk.gov.hmrc.play.scheduling.{ RunningOfScheduledJobs, ScheduledJob }
@@ -55,6 +56,7 @@ trait Scheduler extends RunningOfScheduledJobs {
     maybeInitScheduler(EvaluatePhase1ResultJobConfig, EvaluatePhase1ResultJob),
     maybeInitScheduler(EvaluatePhase2ResultJobConfig, EvaluatePhase2ResultJob),
     maybeInitScheduler(EvaluatePhase3ResultJobConfig, EvaluatePhase3ResultJob),
+    maybeInitScheduler(EvaluateAssessmentScoreJobConfig, EvaluateAssessmentScoreJob),
     maybeInitScheduler(FixerJobConfig, FixerJob),
     maybeInitScheduler(ProgressSdipForFaststreamCandidateJobConfig, ProgressSdipForFaststreamCandidateJob),
     maybeInitScheduler(SiftConfig, ProgressToSiftJob),
