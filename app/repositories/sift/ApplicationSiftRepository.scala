5dcfe9b45019ef581fc88a1b98fdf69b6e2d1d95
Benji Stephenson <bjames.stephenson@gmail.com>
2017-08-21 16:56:46 +0100

    FSET-1739 Progress SdipFaststream candidates through to sift.

--- app/repositories/sift/ApplicationSiftRepository.scala
+++ app/repositories/sift/ApplicationSiftRepository.scala
@@ -74,8 +74,8 @@ class ApplicationSiftMongoRepository(
       ApplicationForSift(applicationId, appStatus, currentSchemeStatus)
     }
 
-    val fsQuery = BSONDocument("$and" -> BSONArray(
-      BSONDocument("applicationRoute" -> ApplicationRoute.Faststream),
+    val fsQuery = (route: ApplicationRoute) => BSONDocument("$and" -> BSONArray(
+      BSONDocument("applicationRoute" -> route),
       BSONDocument("applicationStatus" -> prevPhase),
       BSONDocument(s"testGroups.$prevTestGroup.evaluation.result" -> BSONDocument("$elemMatch" ->
         BSONDocument("schemeId" -> BSONDocument("$in" -> siftableSchemeIds),
@@ -88,7 +88,8 @@ class ApplicationSiftMongoRepository(
     )
 
     val eligibleForSiftQuery = BSONDocument("$or" -> BSONArray(
-      fsQuery,
+      fsQuery(ApplicationRoute.Faststream),
+      fsQuery(ApplicationRoute.SdipFaststream),
       xdipQuery(ApplicationRoute.Edip),
       xdipQuery(ApplicationRoute.Sdip)
     ))
