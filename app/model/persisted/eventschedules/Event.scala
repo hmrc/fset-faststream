88c70a187da78b2e5876781d37bae06212351f32
fayimora <fayi@fayimora.com>
2017-09-04 10:50:28 +0100

    Add logic for updating events

--- app/model/persisted/eventschedules/Event.scala
+++ app/model/persisted/eventschedules/Event.scala
@@ -39,7 +39,21 @@ case class Event(
   skillRequirements: Map[String, Int],
   sessions: List[Session],
   wasBulkUploaded: Boolean = false
-)
+) {
+  def session(sessionId: String): Session = {
+    sessions.find(_.id == sessionId).getOrElse(throw new Exception(s"Unable to find session with ID $sessionId"))
+  }
+}
+
+case class UpdateEvent(id: String, skillRequirements: Map[String, Int], sessions: Seq[UpdateSession]) {
+  def session(sessionId: String): UpdateSession = {
+    sessions.find(_.id == sessionId).getOrElse(throw new Exception(s"Unable to find session with ID $sessionId"))
+  }
+}
+
+object UpdateEvent {
+  implicit val format = Json.format[UpdateEvent]
+}
 
 object Event {
   implicit val eventFormat: OFormat[Event] = Json.format[Event]
