6f9f4886d4d937e4c9c2910371353dd6281f508f
Andrew Rembrandt <andrew@rembrandt.me.uk>
2017-09-08 18:23:26 +0100

    fset 1780 - Only progress to fsac on scheme withdrawl if forms are submitted.

--- app/services/application/ApplicationService.scala
+++ app/services/application/ApplicationService.scala
@@ -117,7 +117,7 @@ trait ApplicationService extends EventSink with CurrentSchemeStatusHelper {
 
     def maybeProgressToFSAC(schemeStatus: Seq[SchemeEvaluationResult], latestProgressStatus: Option[ProgressStatus]) = {
       val greenSchemes = schemeStatus.collect { case s if s.result == Green.toString => s.schemeId }.toSet
-      val shouldProgressToFSAC = !latestProgressStatus.contains(ProgressStatuses.SIFT_ENTERED) &&
+      val shouldProgressToFSAC = !latestProgressStatus.contains(ProgressStatuses.SIFT_READY) &&
         (greenSchemes subsetOf schemesRepo.nonSiftableSchemeIds.toSet)
 
       if (shouldProgressToFSAC) {
