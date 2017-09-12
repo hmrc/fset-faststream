dd03b2e94b1b947f00557f869bd76d2239ea0bf7
Miguel <mgilgar@gmail.com>
2017-09-12 17:04:00 +0100

    [FSET-1781] Manage existing FS user account (email only) - audit

--- test/controllers/AssessorControllerSpec.scala
+++ test/controllers/AssessorControllerSpec.scala
@@ -25,6 +25,7 @@ import org.mockito.ArgumentMatchers.{ eq => eqTo, _ }
 import org.mockito.Mockito._
 import play.api.libs.json.Json
 import play.api.test.Helpers._
+import services.AuditService
 import services.assessor.AssessorService
 import testkit.MockitoImplicits._
 import testkit.UnitWithAppSpec
@@ -32,42 +33,47 @@ import testkit.UnitWithAppSpec
 import scala.concurrent.Future
 
 class AssessorControllerSpec extends UnitWithAppSpec {
-  val mockAssessorService = mock[AssessorService]
-
-  val controller = new AssessorController {
-    override val assessorService = mockAssessorService
-  }
 
   "save assessor" must {
-    "return OK " +
-      "when save is successful" in {
+    "return OK and log AssessorSaved audit event" +
+      "when save is successful" in new TestFixture {
       val Request = fakeRequest(AssessorExamples.Assessor1)
       when(mockAssessorService.saveAssessor(eqTo(AssessorExamples.UserId1), eqTo(AssessorExamples.Assessor1))).thenReturn(Future.successful(()))
+
       val response = controller.saveAssessor(AssessorExamples.UserId1)(Request)
+
       status(response) mustBe OK
+      val logDetails = Map("assessor" -> AssessorExamples.Assessor1.toString)
+      verify(mockAuditService).logEvent(eqTo("AssessorSaved"), eqTo(logDetails))(any(), any())
     }
 
     "return FAILED_DEPENDENCY " +
-      "when there is a CannotUpdateAssessorWhenSkillsAreRemovedAndFutureAllocationExistsException" in {
+      "when there is a CannotUpdateAssessorWhenSkillsAreRemovedAndFutureAllocationExistsException" in new TestFixture {
       val Request = fakeRequest(AssessorExamples.Assessor1)
       when(mockAssessorService.saveAssessor(eqTo(AssessorExamples.UserId1), eqTo(AssessorExamples.Assessor1))).thenReturn(
         Future.failed(new CannotUpdateAssessorWhenSkillsAreRemovedAndFutureAllocationExistsException("", "")))
+
       val response = controller.saveAssessor(AssessorExamples.UserId1)(Request)
+
       status(response) mustBe FAILED_DEPENDENCY
+      verify(mockAuditService, never()).logEvent(any(), any())(any(), any())
     }
 
     "return CONFLICT " +
-      "when there is a OptimisticLockException" in {
+      "when there is a OptimisticLockException" in new TestFixture {
       val Request = fakeRequest(AssessorExamples.Assessor1)
       when(mockAssessorService.saveAssessor(eqTo(AssessorExamples.UserId1), eqTo(AssessorExamples.Assessor1))).thenReturn(
         Future.failed(new OptimisticLockException("")))
+
       val response = controller.saveAssessor(AssessorExamples.UserId1)(Request)
+
       status(response) mustBe CONFLICT
+      verify(mockAuditService, never()).logEvent(any(), any())(any(), any())
     }
   }
 
   "add availability" must {
-    "return Ok when availability is added" in {
+    "return Ok when availability is added" in new TestFixture {
       when(mockAssessorService.saveAvailability(any[AssessorAvailabilities])).thenReturnAsync()
       val response = controller.saveAvailability()(fakeRequest(AssessorAvailabilitiesSum))
       status(response) mustBe OK
@@ -75,7 +81,7 @@ class AssessorControllerSpec extends UnitWithAppSpec {
   }
 
   "find assessor" must {
-    "return Assessor when is successful" in {
+    "return Assessor when is successful" in new TestFixture {
       when(mockAssessorService.findAssessor(eqTo(AssessorExamples.UserId1))).thenReturn(Future.successful(AssessorExamples.Assessor1))
       val response = controller.findAssessor(AssessorExamples.UserId1)(fakeRequest)
       status(response) mustBe OK
@@ -83,7 +89,7 @@ class AssessorControllerSpec extends UnitWithAppSpec {
       contentAsJson(response) mustBe Json.toJson[Assessor](AssessorExamples.Assessor1)
     }
 
-    "return Not Found when assessor cannot be found" in {
+    "return Not Found when assessor cannot be found" in new TestFixture {
       when(mockAssessorService.findAssessor(UserId)).thenReturn(Future.failed(AssessorNotFoundException(UserId)))
       val response = controller.findAssessor(UserId)(fakeRequest)
       status(response) mustBe NOT_FOUND
@@ -93,14 +99,14 @@ class AssessorControllerSpec extends UnitWithAppSpec {
 
   "find availability" must {
 
-    "return an assessor's availability" in {
+    "return an assessor's availability" in new TestFixture {
       when(mockAssessorService.findAvailability(UserId)).thenReturnAsync(AssessorAvailabilitiesSum)
       val response = controller.findAvailability(UserId)(fakeRequest)
       status(response) mustBe OK
       contentAsJson(response) mustBe Json.toJson[AssessorAvailabilities](AssessorAvailabilitiesSum)
     }
 
-    "return Not Found when availability cannot be found" in {
+    "return Not Found when availability cannot be found" in new TestFixture {
       when(mockAssessorService.findAvailability(UserId)).thenReturn(Future.failed(AssessorNotFoundException(UserId)))
       val response = controller.findAvailability(UserId)(fakeRequest)
       status(response) mustBe NOT_FOUND
@@ -108,14 +114,14 @@ class AssessorControllerSpec extends UnitWithAppSpec {
   }
 
   "count submitted" must {
-    "return zero if there are none submitted" in {
+    "return zero if there are none submitted" in new TestFixture {
       when(mockAssessorService.countSubmittedAvailability()).thenReturnAsync(0)
       val response = controller.countSubmittedAvailability()(fakeRequest)
       status(response) mustBe OK
       contentAsJson(response) mustBe Json.obj("size" -> 0)
     }
 
-    "return five if there are five submitted" in {
+    "return five if there are five submitted" in new TestFixture {
       when(mockAssessorService.countSubmittedAvailability()).thenReturnAsync(5)
       val response = controller.countSubmittedAvailability()(fakeRequest)
       status(response) mustBe OK
@@ -125,30 +131,52 @@ class AssessorControllerSpec extends UnitWithAppSpec {
 
   "removeAssessor" must {
     "return CONFLICT " +
-      "when there is CannotRemoveAssessorWhenFutureAllocationExistsException in" in {
+      "when there is CannotRemoveAssessorWhenFutureAllocationExistsException" in new TestFixture {
       val userId = UniqueIdentifier.randomUniqueIdentifier
       when(mockAssessorService.remove(eqTo(userId))).thenReturn(
         Future.failed(new CannotRemoveAssessorWhenFutureAllocationExistsException("", "")))
+
       val response = controller.removeAssessor(userId)(fakeRequest)
+
       status(response) mustBe CONFLICT
+      response.futureValue
+      verify(mockAuditService, never()).logEvent(any(), any())(any(), any())
     }
 
     "return NOT_FOUND " +
-      "when there is CannotRemoveAssessorWhenFutureAllocationExistsException in" in {
+      "when there is CannotRemoveAssessorWhenFutureAllocationExistsException" in new TestFixture {
       val userId = UniqueIdentifier.randomUniqueIdentifier
       when(mockAssessorService.remove(eqTo(userId))).thenReturn(Future.failed(new AssessorNotFoundException("")))
+
       val response = controller.removeAssessor(userId)(fakeRequest)
+
       status(response) mustBe NOT_FOUND
+      response.futureValue
+      verify(mockAuditService, never()).logEvent(any(), any())(any(), any())
     }
 
-    "return OK " +
-      "when assessor is removed" in {
+    "return OK and log AssessorRemoved audit event" +
+      "when assessor is removed" in new TestFixture {
       val userId = UniqueIdentifier.randomUniqueIdentifier
       when(mockAssessorService.remove(eqTo(userId))).thenReturnAsync()
       val response = controller.removeAssessor(userId)(fakeRequest)
+
       status(response) mustBe OK
+
       response.futureValue
       verify(mockAssessorService).remove(eqTo(userId))
+      val logDetails = Map("userId" -> userId.toString)
+      verify(mockAuditService).logEvent(eqTo("AssessorRemoved"), eqTo(logDetails))(any(), any())
+    }
+  }
+
+  trait TestFixture {
+    val mockAssessorService = mock[AssessorService]
+    val mockAuditService = mock[AuditService]
+
+    val controller = new AssessorController {
+      override val assessorService = mockAssessorService
+      override val auditService = mockAuditService
     }
   }
 }
