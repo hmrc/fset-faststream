a3dd730a8399508f17ad86a4ad22a24fcbca8bd8
Ian Stainer <ian.stainer@valtech.co.uk>
2017-09-25 15:31:56 +0100

    fset-1804 code tidy up

--- app/services/onlinetesting/ApplicationStatusCalculator.scala
+++ app/services/onlinetesting/ApplicationStatusCalculator.scala
@@ -41,26 +41,39 @@ trait ApplicationStatusCalculator {
     case _ => None
   }
 
-  //scalastyle:off cyclomatic.complexity
   private def sdipFaststreamCalc(phase: Phase, originalAppStatus: ApplicationStatus,
     evaluatedSchemes: List[SchemeEvaluationResult]): Option[ProgressStatus] = {
     val sdip = "Sdip"
     val fsResults = evaluatedSchemes.filterNot(_.schemeId == SchemeId(sdip)).map(s => Result(s.result))
     val fsOverallResult = faststreamCalc(phase, originalAppStatus, fsResults)
 
+    def phase1SdipFaststreamNotFailed(result: model.EvaluationResults.Result) =
+      result match {
+        case Amber => Some(PHASE1_TESTS_FAILED_SDIP_AMBER)
+        case _ => Some(PHASE1_TESTS_PASSED)
+      }
+
+    def phase2SdipFaststreamNotFailed(result: model.EvaluationResults.Result) =
+      result match {
+        case Amber => Some(PHASE2_TESTS_FAILED_SDIP_AMBER)
+        case _ => Some(PHASE2_TESTS_PASSED)
+      }
+
+    def phase3SdipFaststreamNotFailed(result: model.EvaluationResults.Result) =
+      result match {
+        case Amber => Some(PHASE3_TESTS_FAILED_SDIP_AMBER)
+        case _ => Some(PHASE3_TESTS_PASSED)
+      }
+
     val sdipResult = evaluatedSchemes.filter(_.schemeId == SchemeId(sdip)).map(s => Result(s.result)).head
     sdipResult match {
-      case Amber if fsOverallResult.contains(PHASE1_TESTS_FAILED) => Some(PHASE1_TESTS_FAILED_SDIP_AMBER)
-      case Green if fsOverallResult.contains(PHASE1_TESTS_FAILED) => Some(PHASE1_TESTS_PASSED)
-      case Amber if fsOverallResult.contains(PHASE2_TESTS_FAILED) => Some(PHASE2_TESTS_FAILED_SDIP_AMBER)
-      case Green if fsOverallResult.contains(PHASE2_TESTS_FAILED) => Some(PHASE2_TESTS_PASSED)
-      case Amber if fsOverallResult.contains(PHASE3_TESTS_FAILED) => Some(PHASE3_TESTS_FAILED_SDIP_AMBER)
-      case Green if fsOverallResult.contains(PHASE3_TESTS_FAILED) => Some(PHASE3_TESTS_PASSED)
+      case Amber | Green if fsOverallResult.contains(PHASE1_TESTS_FAILED) => phase1SdipFaststreamNotFailed(sdipResult)
+      case Amber | Green if fsOverallResult.contains(PHASE2_TESTS_FAILED) => phase2SdipFaststreamNotFailed(sdipResult)
+      case Amber | Green if fsOverallResult.contains(PHASE3_TESTS_FAILED) => phase3SdipFaststreamNotFailed(sdipResult)
       case Amber if fsOverallResult.contains(PHASE3_TESTS_PASSED) => Some(PHASE3_TESTS_PASSED_WITH_AMBER)
       case _ => fsOverallResult
     }
   }
-  //scalastyle:on
 
   private def edipSdipCalc(phase: Phase, originalAppStatus: ApplicationStatus,
     results: List[Result]): Option[ProgressStatus] = (phase, originalAppStatus) match {
