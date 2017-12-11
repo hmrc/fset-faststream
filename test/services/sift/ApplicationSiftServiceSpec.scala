d533d3c1550ae13dfe3395d8f1754ead4cff3b85
fayimora <fayi@fayimora.com>
2017-12-11 14:55:53 +0000

    Move sifting tests to separate test group and remove duplicate test

--- test/services/sift/ApplicationSiftServiceSpec.scala
+++ test/services/sift/ApplicationSiftServiceSpec.scala
@@ -183,8 +183,10 @@ class ApplicationSiftServiceSpec extends ScalaMockUnitWithAppSpec {
         result mustBe candidates
       }
     }
+  }
 
-    "sift and update progress status for a candidate" ignore new SiftUpdateTest {
+  "Sifting" ignore {
+    "sift and update progress status for a candidate" in new SiftUpdateTest {
       (mockSiftRepo.getSiftEvaluations _).expects(appId).returningAsync(Nil)
       val expectedUpdateBson = Seq(
         currentSchemeUpdateBson(schemeSiftResult),
@@ -346,7 +348,7 @@ class ApplicationSiftServiceSpec extends ScalaMockUnitWithAppSpec {
     "sift a candidate with remaining schemes to sift" in new SiftUpdateTest {
       (mockSiftRepo.getSiftEvaluations _).expects(appId).returningAsync(Nil)
 
-       val currentStatus = Seq(
+      val currentStatus = Seq(
         SchemeEvaluationResult(SchemeId("GovernmentSocialResearchService"), EvaluationResults.Green.toString),
         SchemeEvaluationResult(SchemeId("Commercial"), EvaluationResults.Green.toString)
       )
