3288280d0a7be1efc257560f5857657362bb1798
Ian Stainer <ian.stainer@valtech.co.uk>
2018-02-28 13:58:25 +0000

    fset-1935 explicitly ignore the FileUploadRepositorySpec which only fails on Travis but runs locally and on Jenkins

--- it/repositories/fileupload/FileUploadRepositorySpec.scala
+++ it/repositories/fileupload/FileUploadRepositorySpec.scala
@@ -1,5 +1,6 @@
 package repositories.fileupload
 
+import org.scalatest.Tag
 import play.api.libs.iteratee.Iteratee
 import repositories.CollectionNames
 import testkit.MongoRepositorySpec
@@ -10,7 +11,7 @@ class FileUploadRepositorySpec extends MongoRepositorySpec {
   lazy val repository: FileUploadMongoRepository = new FileUploadMongoRepository()
 
   "add" must {
-    "store a file with contentType" ignore {
+    "store a file with contentType" taggedAs TravisIgnore in {
       val testContent = "Test contents".toCharArray.map(_.toByte)
       val testContentType = "application/pdf"
       val id = repository.add(testContentType, testContent).futureValue
@@ -21,7 +22,7 @@ class FileUploadRepositorySpec extends MongoRepositorySpec {
   }
 
   "retrieve" must {
-    "retrieve a file by id" ignore {
+    "retrieve a file by id" taggedAs TravisIgnore in {
       val testContent = "Test contents".toCharArray.map(_.toByte)
       val testContentType = "application/pdf"
       val id = repository.add(testContentType, testContent).futureValue
@@ -31,3 +32,7 @@ class FileUploadRepositorySpec extends MongoRepositorySpec {
     }
   }
 }
+
+// This test fails on Travis but works locally and on jenkins by tagging it so we can
+// explicitly skip it when travis runs
+object TravisIgnore extends Tag("TravisIgnore")
