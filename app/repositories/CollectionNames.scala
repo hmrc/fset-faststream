31c5b35b5ca413a6f147c667f0baae0e53eaf64a
Ian Stainer <ian.stainer@valtech.co.uk>
2018-09-13 11:46:54 +0100

    fset-2159 change the collection name suffix to 19 in preparation for the new campaign go-live

--- app/repositories/CollectionNames.scala
+++ app/repositories/CollectionNames.scala
@@ -18,7 +18,7 @@ package repositories
 
 object CollectionNames {
 
-  val suffixForThisCampaign = "19test"
+  val suffixForThisCampaign = "19"
 
   val APPLICATION = s"application$suffixForThisCampaign"
   val ASSESSOR_ASSESSMENT_SCORES = s"assessor-assessment-scores$suffixForThisCampaign"
