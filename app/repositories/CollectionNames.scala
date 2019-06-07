a008e4c45088bfb8819ca8445484b35d52a28a09
Ian Stainer <ian-stainer@users.noreply.github.com>
2019-06-07 15:43:26 +0100

    fset-2346 configure to allow clear down of 2019 data

--- app/repositories/CollectionNames.scala
+++ app/repositories/CollectionNames.scala
@@ -18,7 +18,7 @@ package repositories
 
 object CollectionNames {
 
-  val suffixForThisCampaign = "19"
+  val suffixForThisCampaign = "20test"
 
   val APPLICATION = s"application$suffixForThisCampaign"
   val ASSESSOR_ASSESSMENT_SCORES = s"assessor-assessment-scores$suffixForThisCampaign"
