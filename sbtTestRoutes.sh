20448a1dd0ce8ab8dc2cfe2183f99aba31161f7b
Miguel Gil <mgilgar@gmail.com>
2019-08-27 15:44:49 +0100

    Reverted change in one file

--- sbtTestRoutes.sh
+++ sbtTestRoutes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sbt -jvm-debug 7281 -J-Dapplication.router=testOnlyDoNotUseInAppConf.Routes -Dhttp.port=9284 -Dplay.filters.headers.contentSecurityPolicy='www.google-analytics.com'
+sbt -J-Dapplication.router=testOnlyDoNotUseInAppConf.Routes -Dhttp.port=9284 -Dplay.filters.headers.contentSecurityPolicy='www.google-analytics.com'
