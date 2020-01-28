7c8a92a389fa923487e7e0fbbbee13794c32d058
Ian Stainer <ian-stainer@users.noreply.github.com>
2020-01-28 10:05:04 +0000

    fset-2506 display correct eligible scheme after EAC_DS fsb

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 HM Revenue & Customs
+ * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -146,7 +146,9 @@ abstract class HomeController(
       val phase1DataOpt = phase1TestsWithNames.map(Phase1TestsPage(_))
       val phase2DataOpt = phase2TestsWithNames.map(Phase2TestsPage2(_, None))
       val phase3DataOpt = phase3Tests.map(Phase3TestsPage(_, None))
-
+//scalastyle:off
+      println(">>>>>>>> displayPostOnlineTestsDashboard")
+//scalastyle:on
       val page = PostOnlineTestsPage(
         CachedUserWithSchemeData(cachedData.user, application, schemePreferences, allSchemes, phase3Evaluation, siftEvaluation, schemeStatus),
         allocationWithEvents,
--- app/helpers/CachedUserWithSchemeData.scala
+++ app/helpers/CachedUserWithSchemeData.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 HM Revenue & Customs
+ * Copyright 2020 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,6 +51,9 @@ class CachedUserWithSchemeData(
 
   private val rawSchemeStatusAllGreen = rawSchemesStatus.map(schemeResult =>
     SchemeEvaluationResult(schemeResult.schemeId, SchemeStatus.Green.toString))
+
+  lazy val greenSchemesForDisplay: Seq[CurrentSchemeStatus] =
+    currentSchemesStatus.filter(_.status == SchemeStatus.Green)
 
   lazy val greenAndAmberSchemesForDisplay: Seq[CurrentSchemeStatus] =
     currentSchemesStatus
