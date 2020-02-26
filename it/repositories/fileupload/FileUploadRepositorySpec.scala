c937aeca564c42313a57107f9ba390ef7b38a9bc
Ian Stainer <ian-stainer@users.noreply.github.com>
2020-02-26 10:28:52 +0000

    fset-2504 corrected lib dependencies to fix streaming issue

--- it/repositories/fileupload/FileUploadRepositorySpec.scala
+++ it/repositories/fileupload/FileUploadRepositorySpec.scala
@@ -10,19 +10,8 @@ class FileUploadRepositorySpec extends MongoRepositorySpec {
   override val collectionName: String = CollectionNames.FILE_UPLOAD
   lazy val repository: FileUploadMongoRepository = new FileUploadMongoRepository()
 
-  "add" must {
-    "store a file with contentType" taggedAs TravisIgnore in {
-      val testContent = "Test contents".toCharArray.map(_.toByte)
-      val testContentType = "application/pdf"
-      val id = repository.add(testContentType, testContent).futureValue
-      val retrievedFile = repository.retrieve(id).futureValue
-      retrievedFile.contentType mustBe testContentType
-      retrievedFile.fileContents.run(Iteratee.consume[Array[Byte]]()).futureValue mustBe testContent
-    }
-  }
-
-  "retrieve" must {
-    "retrieve a file by id" taggedAs TravisIgnore in {
+  "add and retrieve" must {
+    "store a file with contentType and fetch it" taggedAs TravisIgnore in {
       val testContent = "Test contents".toCharArray.map(_.toByte)
       val testContentType = "application/pdf"
       val id = repository.add(testContentType, testContent).futureValue
