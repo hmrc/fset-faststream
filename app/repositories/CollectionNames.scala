1b8e990f48a021f923d859550ea7d40a34457388
Ian Stainer <ian-stainer@users.noreply.github.com>
2020-09-23 13:58:28 +0100

    fset-2610 set final collection names for campaign go-live

--- app/repositories/CollectionNames.scala
+++ app/repositories/CollectionNames.scala
@@ -18,7 +18,7 @@ package repositories
 
 object CollectionNames {
 
-  val suffixForThisCampaign = "21Test"
+  val suffixForThisCampaign = "21"
 
   val APPLICATION = s"application$suffixForThisCampaign"
   val ASSESSOR_ASSESSMENT_SCORES = s"assessor-assessment-scores$suffixForThisCampaign"
