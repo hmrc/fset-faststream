fc8bb5cf1951df95634b3239e204d14916e47376
Ian Stainer <ian-stainer@users.noreply.github.com>
2021-06-14 09:08:59 +0100

    fset-2748 fixed date issue in StcEvent

--- app/model/persisted/StcEvent.scala
+++ app/model/persisted/StcEvent.scala
@@ -17,8 +17,7 @@
 package model.persisted
 
 import org.joda.time.DateTime
-import play.api.libs.json.JodaWrites._ // This is needed for DateTime serialization
-import play.api.libs.json.JodaReads._ // This is needed for DateTime serialization
+import uk.gov.hmrc.mongo.play.json.formats.MongoJodaFormats.Implicits._ // Needed to handle storing ISODate format
 import play.api.libs.json.Json
 
 case class StcEvent(
