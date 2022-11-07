f16d9bc048a7122a8358fbc10dc11acf1e7b054e
Ian Stainer <ian-stainer@users.noreply.github.com>
2022-11-07 10:50:31 +0000

    fset-2973 setup service name for 22/23 campaign and collection name change

--- app/repositories/CollectionNames.scala
+++ app/repositories/CollectionNames.scala
@@ -18,7 +18,7 @@ package repositories
 
 object CollectionNames {
 
-  val suffixForThisCampaign = "22"
+  val suffixForThisCampaign = "23Test"
 
   val APPLICATION = s"application$suffixForThisCampaign"
   val ASSESSOR_ASSESSMENT_SCORES = s"assessor-assessment-scores$suffixForThisCampaign"
